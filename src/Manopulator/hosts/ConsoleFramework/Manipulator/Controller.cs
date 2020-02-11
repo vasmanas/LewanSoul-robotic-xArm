528c0943194c52d8c374663de60f07cce3c6e036
Andrius <andrius@lympo.io>
2020-02-11 20:02:44 +0200

    Makings of controller

---
+++ src/Manopulator/hosts/ConsoleFramework/Manipulator/Controller.cs
@@ -0,0 +1,137 @@
+﻿using HidLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleFramework.Manipulator
+{
+    public class Controller : IDisposable
+    {
+        private const int vid = 0x0483;
+        private const int pid = 0x5750;
+
+        private HidDevice device;
+
+        public HidDevice Device
+        {
+            get {
+                return device;
+            }
+        }
+
+        public Controller()
+        {
+            device = HidDevices.Enumerate(vid, pid).FirstOrDefault();
+            if (device == null)
+            {
+                Console.WriteLine("xArm not found.");
+                Console.ReadLine();
+
+                return;
+            }
+
+            Console.WriteLine("xArm connected.");
+
+            // TODO: Is this needed?
+            Thread.Sleep(1000);
+
+            device.OpenDevice();
+            device.Inserted += Device_Inserted;
+            device.Removed += Device_Removed;
+            device.MonitorDeviceEvents = true;
+        }
+
+        public async Task MultiServoMove(
+            ushort milliseconds,
+            ushort? servo1 = null,
+            ushort? servo2 = null,
+            ushort? servo3 = null,
+            ushort? servo4 = null,
+            ushort? servo5 = null,
+            ushort? servo6 = null)
+        {
+            var buffer = new byte[device.Capabilities.OutputReportByteLength];
+
+            using (MemoryStream ms = new MemoryStream(buffer))
+            using (BinaryWriter bw = new BinaryWriter(ms))
+            {
+                bw.Write((byte)0);
+                bw.Write((ushort)0x5555);
+
+                var servoCount =
+                    (servo1 == null ? 0 : 1)
+                    + (servo2 == null ? 0 : 1)
+                    + (servo3 == null ? 0 : 1)
+                    + (servo4 == null ? 0 : 1)
+                    + (servo5 == null ? 0 : 1)
+                    + (servo6 == null ? 0 : 1);
+
+                var length = 1 + 1 + 1 + 2 + servoCount * 3;
+
+                bw.Write((byte)length); // Length
+                bw.Write((byte)RobotCommand.MultiServoMove); // Command: MultiServoMove = 3
+
+                bw.Write((byte)servoCount); // (byte)count
+                bw.Write((ushort)milliseconds); // (ushort)milliseconds
+
+                if (servo1 != null)
+                {
+                    bw.Write((byte)1); // (byte)servo
+                    bw.Write((ushort)servo1); // (ushort)position[FF00 = null]
+                }
+                if (servo2 != null)
+                {
+                    bw.Write((byte)2); // (byte)servo
+                    bw.Write((ushort)servo2); // (ushort)position[FF00 = null]
+                }
+                if (servo3 != null)
+                {
+                    bw.Write((byte)3); // (byte)servo
+                    bw.Write((ushort)servo3); // (ushort)position[FF00 = null]
+                }
+                if (servo4 != null)
+                {
+                    bw.Write((byte)4); // (byte)servo
+                    bw.Write((ushort)servo4); // (ushort)position[FF00 = null]
+                }
+                if (servo5 != null)
+                {
+                    bw.Write((byte)5); // (byte)servo
+                    bw.Write((ushort)servo5); // (ushort)position[FF00 = null]
+                }
+                if (servo6 != null)
+                {
+                    bw.Write((byte)6); // (byte)servo
+                    bw.Write((ushort)servo6); // (ushort)position[FF00 = null]
+                }
+            }
+
+            var ss = await device.WriteAsync(buffer, 300);
+            Console.WriteLine(ss ? "Report sent." : "Read timeout.");
+
+            await Task.Delay(milliseconds + 10);
+        }
+
+        private void Device_Removed()
+        {
+            Console.WriteLine("xArm removed.");
+        }
+
+        private void Device_Inserted()
+        {
+            Console.WriteLine("xArm inserted.");
+        }
+
+        public void Dispose()
+        {
+            if (device != null && device.IsOpen)
+            {
+                device.CloseDevice();
+            }
+        }
+    }
+}
--- src/Manopulator/hosts/ConsoleFramework/Program.cs
+++ src/Manopulator/hosts/ConsoleFramework/Program.cs
@@ -2,36 +2,24 @@
 {
     using System;
     using System.IO;
-    using System.Linq;
     using System.Threading;
+    using System.Threading.Tasks;
+    using ConsoleFramework.Manipulator;
     using HidLibrary;
 
     public class Program
     {
-        private const int vid = 0x0483;
-        private const int pid = 0x5750;
-
-        private static HidDevice device;
-
-        static async System.Threading.Tasks.Task Main(string[] args)
+        static async Task Main(string[] args)
         {
-            device = HidDevices.Enumerate(vid, pid).FirstOrDefault();
-            if (device == null) {
-                Console.WriteLine("xArm not found.");
-                Console.ReadLine();
+            var device = new Controller();
 
-                return;
-            }
+            await device.MultiServoMove(1000, servo1: 0);
+            await device.MultiServoMove(1000, servo1: 650);
+            await device.MultiServoMove(1000, servo1: 650);
+            await device.MultiServoMove(1000, servo1: 650);
+            await device.MultiServoMove(1000, servo1: 650);
 
-            Console.WriteLine("xArm connected.");
-            Thread.Sleep(1000);
-
-            device.OpenDevice();
-            device.Inserted += Device_Inserted;
-            device.Removed += Device_Removed;
-            device.MonitorDeviceEvents = true;
-
-            byte[] buffer = new byte[device.Capabilities.OutputReportByteLength];
+            var buffer = new byte[device.Device.Capabilities.OutputReportByteLength];
 
             using (MemoryStream ms = new MemoryStream(buffer))
             using (BinaryWriter bw = new BinaryWriter(ms))
@@ -39,98 +27,35 @@
                 bw.Write((byte)0);
                 bw.Write((ushort)0x5555);
 
-                bw.Write((byte)10); // Length
-                //bw.Write((byte)0x03); /* 1 */ // Command: MultiServoMove = 3
-                bw.Write((byte)RobotCommand.MultiServoMove); /* 1 */ // Command: MultiServoMove = 3
-
-                bw.Write((byte)0x02); /* 1 */ // (byte)count
-                bw.Write((ushort)1000); /* 2 */ // (ushort)milliseconds
-
-                bw.Write((byte)1); /* 1 */ // (byte)servo
-                bw.Write((ushort)0x0000); /* 2 */ // (ushort)position[FF00 = null]
-
-                bw.Write((byte)2); /* 1 */ // (byte)servo
-                bw.Write((ushort)0x0000); /* 2 */ // (ushort)position[FF00 = null]
-            }
-
-            var ss = await device.WriteAsync(buffer, 300);
-            Console.WriteLine(ss ? "Report sent." : "Read timeout.");
-            //device.Write(buffer, (success) => { Console.WriteLine(success ? "Report sent." : "Read timeout."); }, 300);
-
-            Thread.Sleep(1000);
-
-            Console.WriteLine("Here 1");
-            using (MemoryStream ms = new MemoryStream(buffer))
-            using (BinaryWriter bw = new BinaryWriter(ms))
-            {
-                bw.Write((byte)0);
-                bw.Write((ushort)0x5555);
-
-                bw.Write((byte)10); // Length
-                //bw.Write((byte)0x03); /* 1 */ // Command: MultiServoMove = 3
-                bw.Write((byte)RobotCommand.MultiServoMove); /* 1 */ // Command: MultiServoMove = 3
-
-                bw.Write((byte)0x02); /* 1 */ // (byte)count
-                bw.Write((ushort)2000); /* 2 */ // (ushort)milliseconds
-
-                bw.Write((byte)1); /* 1 */ // (byte)servo
-                bw.Write((ushort)0x0A99); /* 2 */ // (ushort)position[FF00 = null]
-
-                bw.Write((byte)2); /* 1 */ // (byte)servo
-                bw.Write((ushort)0x0A99); /* 2 */ // (ushort)position[FF00 = null]
-            }
-            Console.WriteLine("Here 2");
-
-            device.Write(buffer, (success) => { Console.WriteLine(success ? "Report sent." : "Read timeout."); }, 300);
-
-            Thread.Sleep(2000);
-
-            Console.WriteLine("Here 3");
-            using (MemoryStream ms = new MemoryStream(buffer))
-            using (BinaryWriter bw = new BinaryWriter(ms))
-            {
-                bw.Write((byte)0);
-                bw.Write((ushort)0x5555);
-
-                bw.Write((byte)6); // Length
+                bw.Write((byte)9); // Length
                 bw.Write((byte)RobotCommand.ServoPositionRead); /* 1 */ // Command: ServoPositionRead = 21
 
-                bw.Write((byte)3); /* 1 */ // (byte)count
+                bw.Write((byte)6); /* 1 */ // (byte)count
                 bw.Write((byte)1); /* 1 */ // (byte)servo
                 bw.Write((byte)2); /* 1 */ // (byte)servo
                 bw.Write((byte)3); /* 1 */ // (byte)servo
+                bw.Write((byte)4); /* 1 */ // (byte)servo
+                bw.Write((byte)5); /* 1 */ // (byte)servo
+                bw.Write((byte)6); /* 1 */ // (byte)servo
             }
-            Console.WriteLine("Here 4");
 
-            device.Write(buffer, (success) => { Console.WriteLine(success ? "Report sent." : "Read timeout."); }, 300);
+            device.Device.Write(buffer, (success) => { Console.WriteLine(success ? "Report sent." : "Read timeout."); }, 300);
 
             Console.WriteLine($"{buffer.Length} bytes transmitted.");
 
-            device.ReadReport(ReadReport_EventHandler);
+            //device.Device.ReadReport(ReadReport_EventHandler);
 
             Console.ReadLine();
-
-            device.CloseDevice();
         }
 
-        private static void ReadReport_EventHandler(HidReport report)
-        {
-            Console.WriteLine($"{report.Data.Length} bytes received.");
-            int length = report.Data[3] + 3;
-            byte[] data = new byte[length];
-            Array.Copy(report.Data, data, length);
-            Console.WriteLine(BitConverter.ToString(data));
-            device.ReadReport(ReadReport_EventHandler);
-        }
-
-        private static void Device_Removed()
-        {
-            Console.WriteLine("xArm removed.");
-        }
-
-        private static void Device_Inserted()
-        {
-            Console.WriteLine("xArm inserted.");
-        }
+        //private static void ReadReport_EventHandler(HidReport report)
+        //{
+        //    Console.WriteLine($"{report.Data.Length} bytes received.");
+        //    int length = report.Data[3] + 3;
+        //    byte[] data = new byte[length];
+        //    Array.Copy(report.Data, data, length);
+        //    Console.WriteLine(BitConverter.ToString(data));
+        //    device.ReadReport(ReadReport_EventHandler);
+        //}
     }
 }
