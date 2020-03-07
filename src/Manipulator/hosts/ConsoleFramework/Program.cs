0bea1b09993ce4bd25f46bf95d1ecc039398668a
Andrius <andrius@lympo.io>
2020-03-07 20:57:38 +0200

    add links

--- src/Manipulator/hosts/ConsoleFramework/Program.cs
+++ src/Manipulator/hosts/ConsoleFramework/Program.cs
@@ -6,8 +6,8 @@
     using ConsoleFramework.Manipulator;
     using HidLibrary;
 
+    // https://github.com/ccourson/LewanSoul-xArm
     // https://keisan.casio.com/exec/system/1273849674
-    // https://en.wikipedia.org/wiki/Law_of_cosines
     // https://www.math10.com/en/geometry/geogebra/geogebra.html
     // http://latex.codecogs.com/eqneditor/editor.php
 
