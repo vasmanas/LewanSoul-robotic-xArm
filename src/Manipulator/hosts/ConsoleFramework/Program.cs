0101d6613e95793cb890b66d146fbaa0618a7b18
Andrius <andrius@lympo.io>
2020-03-16 16:42:50 +0200

    Testing lib

--- src/Manipulator/hosts/ConsoleFramework/Program.cs
+++ src/Manipulator/hosts/ConsoleFramework/Program.cs
@@ -23,33 +23,70 @@
 
             var device = new Controller();
 
-            //await device.MultiServoMove(1000, servo1: 0, servo2: 0, servo3: 500, servo6: 1000);
-            //await device.MultiServoMove(1000, servo1: 500, servo2: 1000, servo3: 0, servo6: 0);
-            //await device.MultiServoMove(1000, servo1: 200, servo2: 0, servo3: 700, servo6: 1000);
-            //await device.MultiServoMove(1000, servo1: 700, servo2: 1000, servo3: 200, servo6: 0);
-            //await device.MultiServoMove(1000, servo1: 300, servo2: 0, servo3: 1000, servo6: 1000);
-            //await device.MultiServoMove(1000, servo1: 1000, servo2: 1000, servo3: 0, servo6: 0);
-
             var positions = await device.ServoPositionRead();
             Console.WriteLine(string.Join(",", positions));
 
-            await Program.PointUp(device);
+            //await Program.PointUp(device);
 
-            for (int i = 0; i < 10; i++)
-            {
-                await device.MultiServoMove(250, 700, 700, 613, 179, 738, 512);
-                await device.MultiServoMove(250, 700, 700, 570, 203, 799, 456);
-                await device.MultiServoMove(250, 700, 700, 542, 279, 896, 456);
-                await device.MultiServoMove(250, 700, 700, 542, 376, 981, 478);
-                await device.MultiServoMove(250, 700, 700, 521, 390, 995, 521);
-                await device.MultiServoMove(250, 700, 700, 522, 356, 972, 565);
-                await device.MultiServoMove(250, 700, 700, 538, 300, 916, 591);
-                await device.MultiServoMove(250, 700, 700, 565, 171, 761, 591);
-            }
+            //await device.MultiServoMove(1000, servo4: 400);
+            //await device.MultiServoMove(1000, servo4: 600);
+            //await device.MultiServoMove(1000, servo4: 500);
 
-            await Program.PointUp(device);
+            await device.MultiServoMove(1000, 700, 700, 350, 200, 600, 500);
 
-            //await device.MultiServoMove(1000, servo2: 1000);
+            positions = await device.ServoPositionRead();
+            Console.WriteLine(string.Join(",", positions));
+
+            // a = 98 mm
+            // b = 96 mm
+            // positions[4]: servo5[100, 500, 850] => ad(0, 90, 180)
+            // positions[3]: servo4[100, 500, 850] => ba(90, 180, 270)
+
+            // positions[3]: servo4[100, 500, 850] => ba(90, 180, 270)
+            Func<double, double> calcba = pos =>
+                (pos <= 500 ? (90 * (pos - 100)) / 400 : ((90 * (pos - 500)) / 350) + 90) + 90;
+
+            // positions[4]: servo5[100, 500, 850] => ad(0, 90, 180)
+            Func<double, double> calcad = pos =>
+                pos <= 500 ? (90 * (pos - 100)) / 400 : ((90 * (pos - 500)) / 350) + 90;
+
+            var ba = calcba(positions[3]);
+            var ad = calcad(positions[4]);
+
+            var quadInit = Quadrilateral.WithTwoSidesTwoAngles(98, 96, ad, ba);
+            var quadDelta = Quadrilateral.WithFourSides(98, 96, quadInit.SideC - 50, quadInit.SideD); // pass
+
+            Console.WriteLine(quadDelta.AngleAD);
+            Console.WriteLine(quadDelta.AngleBA);
+
+            // ba(90, 180, 270) => servo4[100, 500, 850]
+            Func<double, double> calcServo4 = angle =>
+                angle < 180 ? (400 * (angle - 90) / 90) + 100 : (350 * (angle - 180) / 90) + 500;
+
+            // ad(0, 90, 180) => servo5[100, 500, 850]
+            Func<double, double> calcServo5 = angle =>
+                angle < 90 ? (400 * angle / 90) + 100 : (350 * (angle - 90) / 90) + 500;
+
+            await device.MultiServoMove(
+                1000,
+                servo4: (ushort)calcServo4(quadDelta.AngleBA),
+                servo5: (ushort)calcServo5(quadDelta.AngleAD)
+            );
+            
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    await device.MultiServoMove(250, 700, 700, 613, 179, 738, 512);
+            //    await device.MultiServoMove(250, 700, 700, 570, 203, 799, 456);
+            //    await device.MultiServoMove(250, 700, 700, 542, 279, 896, 456);
+            //    await device.MultiServoMove(250, 700, 700, 542, 376, 981, 478);
+            //    await device.MultiServoMove(250, 700, 700, 521, 390, 995, 521);
+            //    await device.MultiServoMove(250, 700, 700, 522, 356, 972, 565);
+            //    await device.MultiServoMove(250, 700, 700, 538, 300, 916, 591);
+            //    await device.MultiServoMove(250, 700, 700, 565, 171, 761, 591);
+            //}
+
+            //await Program.PointUp(device);
+
             //Console.WriteLine(string.Join(",", await device.ServoPositionRead()));
 
             Console.ReadLine();
@@ -61,10 +98,10 @@
             return device.MultiServoMove(
                 1000,
                 servo1: 700, // [140(Open), 700(Closed)]
-                servo2: 700, // [0, 1000]
-                servo3: 400, // [0, 1000]
-                servo4: 500, // [0, 1000]
-                servo5: 850, // [0, 1000]
+                servo2: 680, // [0, 1000]
+                servo3: 500, // [90 - 150, 180 - 500, 270 - 900]
+                servo4: 500, // [90 - 100, 180 - 500, 270 - 850]
+                servo5: 500, // [0 - 100, 90 - 500, 180 - 850]
                 servo6: 500); // [0, 1000]
         }
     }
