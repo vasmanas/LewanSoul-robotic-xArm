3afe33ade9f09cd9bed2c91fb6c2c78a17c06f78
Andrius <andrius@lympo.io>
2020-04-05 14:43:25 +0300

    Add angle class

---
+++ src/Manipulator/hosts/ConsoleFramework/Utils/Angle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFramework.Utils
+{
+    public class Angle
+    {
+        private double radians;
+
+        public Angle(double radians)
+        {
+            this.radians = radians;
+        }
+
+        public double Radians
+        {
+            get => this.radians;
+            set => this.radians = value;
+        }
+
+        public double Degrees
+        {
+            get => Converter.RadiansToDegrees(this.radians);
+            set => this.radians = Converter.DegreesToRadians(value);
+        }
+    }
+}
